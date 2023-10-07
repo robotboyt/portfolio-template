f0c07e325fc32cbf4a597c672a074eda9b7cd4f1
robotboyt <bokachodantes@gmail.com>
2023-10-08 01:03:36 +0200

    added mui

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "../../App.module.css";
 import avatar from "../../assets/avatar.png";
+import { SvgIcon } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
 
 const Profile = () => {
+  let icons = [LinkedInIcon, GitHubIcon, AlternateEmailIcon, ContactPhoneIcon];
+
   return (
     <div className={styles.profile}>
       <div className={styles.profileBlock}>
@@ -15,7 +22,11 @@ const Profile = () => {
               <h2>Theodor Vazovski</h2>
             </div>
           </div>
-          <div className={styles.profileLinks}></div>
+          <div className={styles.profileLinks}>
+            {icons.map((item) => {
+              return <SvgIcon component={item} sx={{ fontSize: 40 }} />;
+            })}
+          </div>
         </div>
       </div>
     </div>

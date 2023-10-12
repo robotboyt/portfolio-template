e06059e826c9e8d6ac7dbb2c845f37c9d204810e
robotboyt <bokachodantes@gmail.com>
2023-10-12 23:22:49 +0200

    added new component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,17 +3,23 @@ import styles from "../../App.module.css";
 import { SvgIcon } from "@mui/material";
 import ProfileBlock from "../ProfileBlock/ProfileBlock";
 import ProfileExpirience from "../ProfileExpirience/ProfileExpirience";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import ProfileEducation from "../ProfileEducation/ProfileEducation";
 
 const Profile = () => {
   return (
     <>
-      <div className={styles.navbar}>navigation</div>
+      <div className={styles.navbar}>
+        <Link to="/">Expirience</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/about-me">About Me</Link>
+      </div>
       <div className={styles.profile}>
         <ProfileBlock SvgIcon={SvgIcon} />
         <div className={styles.profileSummary}>
           <Routes>
             <Route path="/" element={<ProfileExpirience SvgIcon={SvgIcon} />} />
+            <Route path="/education" element={<ProfileEducation />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/ProfileEducation/ProfileEducation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../App.module.css";
+
+const ProfileEducation = () => {
+  return (
+    <div className={styles.ProfileEducation}>
+      <h2>Education</h2>
+    </div>
+  );
+};
+
+export default ProfileEducation;
